e76150c047d061a51596d01e551b4f5eb05db72e
John Otander <johnotander@gmail.com>
2014-12-23 11:22:58 -0700

    Add thorough component tests

--- tests/unit/components/flex-grid-row-test.js
+++ tests/unit/components/flex-grid-row-test.js
@@ -3,19 +3,19 @@ import {
   test
 } from 'ember-qunit';
 
-moduleForComponent('flex-grid-row', 'FlexGridRowComponent', {
-  // specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
-});
+moduleForComponent('flex-grid-row', 'FlexGridRowComponent', {});
 
 test('it renders', function() {
-  expect(2);
-
-  // creates the component instance
   var component = this.subject();
   equal(component._state, 'preRender');
 
-  // appends the component to the page
   this.append();
   equal(component._state, 'inDOM');
+});
+
+test('it has the correct class', function() {
+  this.subject();
+  var component = this.append();
+
+  ok(component.attr('class').indexOf('g-r') >= 0);
 });
